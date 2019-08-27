e4b2d4e69af3c032d20565fa92cb39a4ef5e4fe8
DittoGod <dittogod1@gmail.com>
2019-08-27 12:42:35 +0100

    connected to mongo database

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
     app = express(),
     port = 4030,
-    campgrounds = [{
-            name: "Salmon Creek",
-            image: "https://photosforclass.com/download/pixabay-691424?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F50e9d4474856b108f5d084609620367d1c3ed9e04e50744f732b72d0904ac1_960.jpg&user=Free-Photos"
-        },
-        {
-            name: "Granite Falls",
-            image: "https://photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=Pexels"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-        {
-            name: "Willow Down Falls",
-            image: "https://photosforclass.com/download/pixabay-4303359?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c73297fdd934ac25c_960.jpg&user=chanwity"
-        },
-    ];
+    // SCHEMA SETUP
+    campgroundSchema = new mongoose.Schema({
+        name: String,
+        image: String,
+    }),
+    Campground = mongoose.model("Campground", campgroundSchema);
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+    useNewUrlParser: true
+});
+
+// campground.create({
+//     name: "Willow Way Creek",
+//     image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg"
+// }, (err, campground) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("newly created campground: ")
+//         console.log(campground);
+//     }
+// });
 
 app.use(bodyParser.urlencoded({
     extended: true,
@@ -54,24 +36,44 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {
-        campgrounds: campgrounds,
-    });
+    // get all campgrounds from DB
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {
+                campgrounds: campgrounds,
+            });
+        }
+    })
 });
 
 app.post('/campgrounds', (req, res) => {
     // get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCamp = {name: name, image: image};
-    campgrounds.push(newCamp);
-    // redirect to campground post.
-    res.redirect('/campgrounds');
+    var name = req.body.name,
+        image = req.body.image,
+        newCamp = {
+            name: name,
+            image: image
+        };
+    // create new campground and save to DB
+    Campground.create(newCamp, (err, campground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("new campground added: ");
+            console.log(campground);
+            // redirect to campground post.
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
 });
+
+
 
 app.listen(port, () => {
     console.log("YelpCamp server has started.");
