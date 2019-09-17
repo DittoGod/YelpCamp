401f6c961ad8aae4f8875e224b97b7822f3d9d2d
dittogod <dittogod1@gmail.com>
2019-09-17 13:41:49 +0100

    added sessions and full* login and logout functionality

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ passport.use(new LocalStratagy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('landing');
     // res.rander('/campgrounds');
